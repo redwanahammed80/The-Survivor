ddeaa2b48d5a046181ada66a737d0f34e0a640af
redwanahammed80 <60567848+redwanahammed80@users.noreply.github.com>
2020-02-11 15:48:44 +0600

    Add files via upload

---
+++ The Survivor/Logs/Assets/MiniMapScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapScript : MonoBehaviour
+{
+    public Transform FPSController;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = FPSController.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+
+        transform.rotation = Quaternion.Euler(90f, FPSController.eulerAngles.y , 0f);
+
+
+    }
+}
